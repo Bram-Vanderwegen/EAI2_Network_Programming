9f7d15007ca9a6973140765d7e502475e5cd3d72
Bram Vanderwegen <12001742@student.pxl.be>
2022-05-08 15:24:04 +0200

    file transfer succesfull

--- Service_2/Sources/main.cpp
+++ Service_2/Sources/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <pthread.h>
 #include <zmq.hpp>
+#include <fstream>
 
 using namespace std;
 
@@ -9,28 +10,41 @@ void *requestSender( void *arg );
 
 int main()
 {
-    FILE * fp;
-    fp = fopen("war_criminal.png", "rb");
-    std::cout << fp->_base << endl;
-    std::cout << fp->_bufsiz << endl;
-    std::cout << fp->_charbuf << endl;
-    std::cout << fp->_cnt << endl;
-    std::cout << fp->_file << endl;
-    std::cout << fp->_flag << endl;
-    std::cout << fp->_ptr << endl;
-    std::cout << fp->_tmpfname << endl;
+    /*ifstream fp;
+    fp.open("war_criminal.bmp", ifstream::binary);
+    if(fp.is_open()){
+            cout << "open" << endl;
+            // checks for length
+            fp.seekg (0, fp.end);
+            int length = fp.tellg();
+            fp.seekg (0, fp.beg);
+            //copys
+            char * buffer = new char[length];
+            fp.read ((char*) &buffer[0] ,length);
+
+            //write data
+            ofstream fpw;
+            fpw.open("copy.bmp", ofstream::binary);
+            if(fpw.is_open()){
+                cout << "open" << endl;
+                for(int i = 0; i < length; i++){
+                    fpw << buffer[i];
+                }
+            }
+            else{
+                cout << "not open" << endl;
+            }
+            delete[] buffer;
+    }*/
 
 
 
 
 
-
-
-
-
-    /*pthread_t thread2;
+    pthread_t thread2;
     pthread_create(&thread2, NULL, requestSender, NULL);
-    pthread_join(thread2, NULL);*/
+    pthread_join(thread2, NULL);
+
     cout << "Hello krokodil!" << endl;
     return 0;
 }
@@ -44,14 +58,42 @@ void *requestSender(void *arg){
     zmq::socket_t pusher( context, ZMQ_PUSH);
     pusher.connect("tcp://benternet.pxl-ea-ict.be:24041");
 
+
+
     while(1)
     {
         try {
             std::string message_tosend;
             std::cin >> message_tosend;
-            message_tosend.insert(0, "");
-            pusher.send(message_tosend.c_str(), message_tosend.size());
-            std::cout << "Pushed : " << message_tosend << std::endl;
+
+            ifstream fp;
+            fp.open(message_tosend, ifstream::binary);
+            if(fp.is_open()){
+                    cout << "open" << endl;
+                    // checks for length
+                    fp.seekg (0, fp.end);
+                    int length = fp.tellg();
+                    fp.seekg (0, fp.beg);
+                    //copys
+                    char * buffer = new char[length];
+                    fp.read ((char*) &buffer[0] ,length);
+
+                    //add delim
+
+                    string message = "bram>image>";
+                    for(int i = 0; i < 100; i++){
+                        message.push_back(buffer[i]);
+                    }
+
+
+                    message_tosend.insert(0, "");
+                    pusher.send(message.c_str(), message.length());
+                    std::cout << "Pushed : " << message_tosend << std::endl;
+                    delete [] buffer;
+            }
+            else{
+                cout << "try again" << endl;
+            }
         }
         catch(zmq::error_t& e) {
             std::cout << e.what() << std::endl;
